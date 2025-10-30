93f29a3e17f79881247d4b7122841077c4c75cd3
Jakub Jareš <me@jakubjares.com>
2025-10-30 16:04:02 +0100

    Add test

---
+++ Colors/Test1.cs
@@ -0,0 +1,13 @@
+﻿namespace Colors
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var actual = ".NET 10";
+            Assert.AreEqual("HELLO!", actual);
+        }
+    }
+}

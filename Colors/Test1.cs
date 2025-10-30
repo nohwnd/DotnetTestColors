c5d0b6e22eca4fea92daa24e96c537f3c3ba0591
Jakub Jareš <me@jakubjares.com>
2025-10-30 16:07:52 +0100

    pipeline

--- Colors/Test1.cs
+++ Colors/Test1.cs
@@ -6,6 +6,16 @@
         [TestMethod]
         public void TestMethod1()
         {
+        }
+
+         [TestMethod]
+        public void TestMethod2()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod3 ()
+        {
             var actual = ".NET 10";
             Assert.AreEqual("HELLO!", actual);
         }
